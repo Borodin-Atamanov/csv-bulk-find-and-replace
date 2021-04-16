14458239c99c6c368e2f55f647cadcbca979dda2
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2021-04-16 15:08:26 +0400

    Apache 2.0

--- csv-bulk-find-and-replace.py
+++ csv-bulk-find-and-replace.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 """
+© Copyright 2021 Slava Borodin-Atamanov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 
 """
 
@@ -11,7 +25,7 @@ from configparser import ConfigParser, ExtendedInterpolation
 import jsonpickle
 
 __author__ = "Slava Borodin-Atamanov"
-__license__ = "GPLv3 (GNU General Public License Version 3)"
+__license__ = "Apache 2.0"
 __maintainer__ = "python@Borodin-Atamanov.ru"
 __copyright__ = "Copyright 2021, Slava Borodin-Atamanov"
 __email__ = "python@Borodin-Atamanov.ru"
